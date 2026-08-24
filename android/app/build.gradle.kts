c2e06825b2413332524c63dc68eca4004e60281f
Atropine3333 <yzy_071018@outlook.com>
2026-08-25 00:49:00 +0800

    build: NDK 升至 28.2（sqlite3_flutter_libs 要求）

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 android {
     namespace = "dev.octo.octo_note"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = "27.2.12479018"
+    ndkVersion = "28.2.13676358"
 
     compileOptions {
         isCoreLibraryDesugaringEnabled = true
